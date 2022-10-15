e6f96713097dd48d2d11bb860c49af6f6330f496
MudK01 <khalid.mueed01@gmail.com>
2022-10-15 13:56:55 -0700

    removed pyodide

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import React, { ReactElement } from "react";
-import scrapeSite from "./webScraping/test";
 
 const App = (): ReactElement => {
   return (
     <div>
-      <body>
-        <h1>HELLO</h1>
-        <button
-          onClick={() =>
-            scrapeSite().then((result: string) => console.log(result))
-          }
-        >
-          Press me to run python
-        </button>
-      </body>
+      <h1>HELLO</h1>
     </div>
   );
 };
